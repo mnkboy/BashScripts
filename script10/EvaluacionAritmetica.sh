8f4f3c9f429a5dd37189aa456216309c87cdc413
Javier Jimenez <desarrolloweb@sistemexico.com>
2020-09-08 18:03:56 -0500

    Se suben los scripts

--- script10/EvaluacionAritmetica.sh
+++ script10/EvaluacionAritmetica.sh
@@ -12,3 +12,14 @@ echo "Resta: $resta"
 echo "Multiplicación: $mul"
 echo "División: $div"
 echo "Módulo: $mod"
+
+# Esta es una comparacion de resultados de una suma
+if [ $suma -gt 0 ]
+    then
+        echo "Es positiva la suma"
+elif [ 0 -gt $suma ]
+    then
+        echo "Es negativa la suma"
+else 
+    echo "Es igual a cero"
+fi
